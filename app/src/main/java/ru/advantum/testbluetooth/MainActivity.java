16998e2269157e233c6b4484520d0c2bfe70b337
Maxim <femax339@gmail.com>
2017-07-20 18:35:36 +0300

    reforamt used library

--- app/src/main/java/ru/advantum/testbluetooth/MainActivity.java
+++ app/src/main/java/ru/advantum/testbluetooth/MainActivity.java
@@ -8,10 +8,12 @@ import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -30,10 +32,12 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "BluetoothActivity";
     private static final int BUFFER_SIZE = 256;
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
     BluetoothAdapter bluetoothAdapter;
     BluetoothSocket socket;
     BluetoothDevice myDevice;
     OutputStream outputStream;
+    TextView textView1;
     private SensorManager sensorManager;
     private BluetoothSocket bluetoothSocket;
     private BluetoothAdapter mBluetoothAdapter;
@@ -42,6 +46,8 @@ public class MainActivity extends Activity {
     private BluetoothSocket mmSocket;
     private OutputStream mmOutputStream;
     private InputStream mmInputStream;
+    private Button textBT;
+    private Handler handler;
 
     public static byte[] SendByteData(String hexString) {
         byte[] sendingThisByteArray = new byte[hexString.length() / 2];
@@ -64,6 +70,15 @@ public class MainActivity extends Activity {
         return sendingThisByteArray;
     }
 
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 
     /**
      * Called when the activity is first created.
@@ -79,60 +94,39 @@ public class MainActivity extends Activity {
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetooth, 0);
         }
-
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            mAdapter = new DeviceAdapter(pairedDevices);
-        }
-
-        mListView = (ListView) findViewById(R.id.listview);
-        final Handler myHandler = new Handler();
-        mListView.setAdapter(mAdapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        textBT = findViewById(R.id.text_button);
+        textView1 = findViewById(R.id.textview);
+        textBT.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
-                try {
-                    mmSocket = mAdapter.getItem(i).createRfcommSocketToServiceRecord(uuid);
-                    mmSocket.connect();
-                    mmInputStream = mmSocket.getInputStream();
-                    new Thread(new Runnable() {
-                        public void run() {
-                            final byte[] buffer = new byte[BUFFER_SIZE];
-                            try {
-                                do {
-                                    if (mmInputStream.read(buffer) == -1) {
-                                        throw new Exception("Error reading data.");
-                                    }
-                                    myHandler.post(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            makeToast(buffer.toString());
-                                        }
-                                    });
-                                } while (true);
-
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                makeToast(e.getMessage());
-                            } catch (Exception e) {
-                                makeToast(e.getMessage());
-
-                            }
-                        }
-                    }).start();
-                    mmOutputStream = mmSocket.getOutputStream();
-
-                    mmOutputStream.write(new byte[]{(byte) 0x05, (byte) 0x00, (byte) 0x31, (byte) 0x32, (byte) 0x33, (byte) 0x34
-                            , (byte) 0xA2, (byte) 0x4b, (byte) 0x64, (byte) 0x1b});
-
+            public void onClick(View view) {
+                if (myDevice == null) {
+                    findBT("VEGA");
+                } else try {
+                    openBT();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
+            }
+        });
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 1) {
+                    String str = (String) msg.obj;
+                    textView1.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
+        Button sendBt = findViewById(R.id.text_send);
+        sendBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    sendData();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -148,6 +142,9 @@ public class MainActivity extends Activity {
             socket = myDevice.createRfcommSocketToServiceRecord(uuid);
             socket.connect();
             outputStream = socket.getOutputStream();
+            mmInputStream = socket.getInputStream();
+            new ConnectedThread(socket).start();
+
 
             textBT.setText("BT Connected");
 
@@ -157,18 +154,20 @@ public class MainActivity extends Activity {
         }
     }
 
-
     void sendData() throws IOException {
-        try {
-            relayID = buttonID + 1; // buttons are 0-7, relays are 1-8
-            relayID = buttonID;
+        String[] size1 = new String[]{"0500", "0400", "0040", "0004"};
+        String type = "05";
+        String key = "31323334";
+        byte[] crc2 = new byte[]{(byte) 0x04, (byte) 0x00, (byte) 0x05, (byte) 0x31, (byte) 0x32, (byte) 0x33, (byte) 0x34
+                , (byte) 0xA2, (byte) 0x4b, (byte) 0x64, (byte) 0x1b};
 
-            String message = Integer.toString(relayID);
 
-            message += "\n";
-            outputStream.write(message.getBytes());
-            textDebug1.setText("Data Sent: " + message);
-        }
+        String message = "\n";
+
+        outputStream.write(crc2);
+
+
+    }
 
     void findBT(String myBTstring) {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -184,7 +183,7 @@ public class MainActivity extends Activity {
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
-                if (device.getName().equals(myBTstring))//
+                if (device.getName().contains(myBTstring))//
                 {
                     myDevice = device;
                     textBT.setText("Looking for: " + myBTstring);
@@ -200,6 +199,40 @@ public class MainActivity extends Activity {
 
     public void makeToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+            }
+            mmInStream = tmpIn;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[5];
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    String str = new String(buffer);
+                    handler.obtainMessage(1, bytes, -1, str).sendToTarget();
+
+                } catch (Exception e) {
+                    System.out.print("read error");
+                    break;
+                }
+
+            }
+        }
     }
 }
 
